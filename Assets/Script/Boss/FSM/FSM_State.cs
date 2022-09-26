c0a44481a5464464f2d62c0aa3a98461ff699602
YunonXD <HowLewd@proton.me>
2022-09-26 13:33:07 +0900

    MainV1p3 Boss/Youn 백업

--- Assets/Script/Boss/FSM/FSM_State.cs
+++
@@ -1,7 +0,0 @@
-
-abstract public class FSM_State<t>
-{
-    abstract public void EnterState(t _Hansel);
-    abstract public void UpdateState(t _Hansel);
-    abstract public void ExitState(t _Hansel);
-}
--- Assets/Script/Interface/IKnockBack.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IKnockBack
-{
-    void KnockBack(Vector2 center, Vector3 power);
-}
